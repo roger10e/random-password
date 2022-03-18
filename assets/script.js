aa59811255328d1a344ae90a1c68f1c0f3991b9b
roger10e <rogertenny@gmail.com>
2022-03-18 15:48:04 -0500

    first try

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
 // Assignment code here
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var special = ",.!@$%^&*'";
+  var allChars = lower + upper + numbers + special;
 
+  var passwordArray = Array(generatePassword.length)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
