99688f4ff20ff876a5b4c64781f24dbf17168c4a
roger10e <rogertenny@gmail.com>
2022-03-19 18:01:03 -0500

    some features added

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,43 @@
 // Assignment code here
-  var lower = "abcdefghijklmnopqrstuvwxyz";
-  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "1234567890";
-  var special = ",.!@$%^&*'";
-  var allChars = lower + upper + numbers + special;
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "1234567890";
+var specialChars = ",.!@$%^&*'";
+var allChars = lowerChars + upperChars + numberChars + specialChars;
 
-  var passwordArray = Array(generatePassword.length)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey, you clicked the button!")
+  
+  //1. Prompt the user for password criteria
+  //1a. Password length between 8 & 128
+  //1b. include lowercase, uppercase, and special characters
+  
+  var passwordLength = Number(window.prompt("How long would you like your password to be?"))
+  console.log(passwordLength)
+  //somehow ensure that the number provided is between 8 and 128 characters?
+
+  var characterChoice = window.prompt("What sort of characters would you like your password to contain (lowercase, uppercase, special)?")
+  console.log(characterChoice)
+  //somehow ensure that the answer provided is valid
+
+
+  //2. Generate password based on criteria
+  
+  //3. Display generated password on the page
+
+
+
+
+  return "Generated password will go here!";
+  
+}
+
 // Write password to the #password input
 function writePassword() {
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
